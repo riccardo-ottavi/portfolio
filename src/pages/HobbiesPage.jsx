82701ad0da4bcd5a40e9572e329c71e5e7898bec
riccardo-ottavi <ottavi.ps@libero.it>
2025-12-10 15:38:11 +0100

    prova alcuni stili

--- src/pages/HobbiesPage.jsx
+++ src/pages/HobbiesPage.jsx
@@ -1,5 +1,27 @@
 export default function HobbiesPage(){
     return (
-        <h2>Sono la pagina con gli Hobbies</h2>
+        <>
+            <h2>Sono la pagina con gli Hobbies</h2>
+            {/*Sezione 1*/}
+            <h3>Sezione</h3>
+            <section className="section-box">
+                <div className="img-box">
+                    <h3>qua la foto</h3>
+                </div>
+                <div className="text-box">
+                    <h3>qua il testo</h3>
+                </div>
+            </section>
+            {/*Sezione 2*/}
+            <h3>Sezione</h3>
+            <section className="section-box">
+                <div className="img-box">
+                    <h3>qua la foto</h3>
+                </div>
+                <div className="text-box">
+                    <h3>qua il testo</h3>
+                </div>
+            </section>
+        </>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,27 @@
 export default function HomePage(){
     return(
-        <h2>Sono l'homepage</h2>
+        <>
+            <h2>Sono l'homepage</h2>
+            {/*Sezione 1*/}
+            <h3>Sezione</h3>
+            <section className="section-box">
+                <div className="img-box">
+                    <h3>qua la foto</h3>
+                </div>
+                <div className="text-box">
+                    <h3>qua il testo</h3>
+                </div>
+            </section>
+            {/*Sezione 2*/}
+            <h3>Sezione</h3>
+            <section className="section-box">
+                <div className="img-box">
+                    <h3>qua la foto</h3>
+                </div>
+                <div className="text-box">
+                    <h3>qua il testo</h3>
+                </div>
+            </section>
+        </>
     )
 }

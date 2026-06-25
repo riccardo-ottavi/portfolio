dbd6e7cd757faa81c923fe890c7e6f7582f31e8b
riccardo-ottavi <ottavi.ps@libero.it>
2026-06-25 12:33:33 +0200

    init instructionInfos

---
+++ src/data/instructionInfos.js
@@ -0,0 +1,35 @@
+const instructionInfos = [
+    {
+        id: 1,
+        name: "Liceo Classico",
+        period: "2011-2016",
+        finalGrade: "X",
+        details: "Lorem ipsum bla bla bla bla bla bla bla bla bla",
+        skills: ["Pensiero critico", "Pensiero analitco"]
+    },
+    {
+        id: 2,
+        name: "Filosofia presso Alma Mater Studiorum Bologna",
+        period: "2017-2021",
+        finalGrade: "84",
+        details: "Lorem ipsum bla bla bla bla bla bla bla bla bla",
+        skills: ["Pensiero critico", "Pensiero analitco"]
+        
+    },
+    {
+        id: 3,
+        name: "Filosofia dell'Informazione presso Carlo Bo Urbino",
+        period: "2021-2024",
+        finalGrade: "105",
+        details: "Lorem ipsum bla bla bla bla bla bla bla bla bla",
+        skills: ["Pensiero critico", "Pensiero analitco"]
+    },
+    {
+        id:4,
+        name: "Master in Web Development 600 ore presso Boolean",
+        period: "07/2025 - 03/2026",
+        finalGrade: "X",
+        details: "Lorem ipsum bla bla bla bla bla bla bla bla bla",
+        skills: ["Pensiero critico", "Pensiero analitco"]
+    }
+]
--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { projects } from "../data/projectsInfos";
+import { Link } from "react-router-dom";
 
 export default function ProjectDetails() {
     const { id } = useParams();
@@ -14,6 +15,7 @@ export default function ProjectDetails() {
 
     return (
         <>
+            <Link to={"/"}><button>Indietro</button></Link>
             <main className="project-detail">
 
                 <div className="container">
